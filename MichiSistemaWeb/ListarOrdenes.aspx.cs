c1de7add78534167261bf0708c301d3f111b9ad8
PapaHuayro <jack.mateo1120@gmail.com>
2025-05-31 19:29:00 -0500

    version v3 Listar ordenes (recuperado borrado de error)

--- MichiSistemaWeb/ListarOrdenes.aspx.cs
+++ MichiSistemaWeb/ListarOrdenes.aspx.cs
@@ -8,10 +8,74 @@ using System.Web.UI.WebControls;
 namespace MichiSistemaWeb
 {
 	public partial class ListarOrdenes : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    {
+		//private EmpleadoBO boEmpleado;
+        //private BindingList<Empleado> empleados;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //boEmpleado = new EmpleadoBO();
+            //empleados = boEmpleado.listarTodos();
+            //dgvEmpleados.DataSource = empleados;
+            dgvOrdenes.DataBind();
+        }
 
-		}
-	}
+        protected void dgvOrdenes_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Cells[0].Text = DataBinder.Eval(e.Row.DataItem, "ID").ToString();
+                e.Row.Cells[1].Text = DataBinder.Eval(e.Row.DataItem, "Tipo recepcion").ToString();
+                e.Row.Cells[2].Text = DataBinder.Eval(e.Row.DataItem, "Set up personalizad").ToString();
+                e.Row.Cells[3].Text = DataBinder.Eval(e.Row.DataItem, "Monto total").ToString();
+                e.Row.Cells[4].Text = DataBinder.Eval(e.Row.DataItem, "Saldo").ToString();
+                e.Row.Cells[5].Text = DataBinder.Eval(e.Row.DataItem, "Cantidad dias").ToString();
+                e.Row.Cells[6].Text = DataBinder.Eval(e.Row.DataItem, "Fecha devolución").ToString();
+                e.Row.Cells[7].Text = DataBinder.Eval(e.Row.DataItem, "Fecha emision").ToString();
+                e.Row.Cells[8].Text = DataBinder.Eval(e.Row.DataItem, "Fecha registro").ToString();
+                e.Row.Cells[9].Text = DataBinder.Eval(e.Row.DataItem, "Fecha entrega").ToString();
+                e.Row.Cells[10].Text = DataBinder.Eval(e.Row.DataItem, "Tipo estado devolución").ToString();
+                e.Row.Cells[11].Text = DataBinder.Eval(e.Row.DataItem, "Id cliente").ToString();
+                e.Row.Cells[12].Text = DataBinder.Eval(e.Row.DataItem, "Id trabajador").ToString();
+            }
+        }
+
+        protected void dgvOrdenes_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvEmpleados.PageIndex = e.NewPageIndex;
+            dgvEmpleados.DataBind();
+        }
+
+        protected void lbRegistrar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegistrarOrden.aspx");
+        }
+
+        protected void lbModificar_Click(object sender, EventArgs e)
+        {
+            int idEmpleado = Int32.Parse(((LinkButton)sender).CommandArgument);
+            //Empleado empleado = empleados.SingleOrDefault(x => x.IdPersona == idEmpleado);
+            // Session["empleadoSeleccionado"] = empleado;
+            Response.Redirect("RegistrarOrden.aspx?accion=modificar");
+        }
+
+        protected void lbEliminar_Click(object sender, EventArgs e)
+        {
+            int idEmpleado = Int32.Parse(((LinkButton)sender).CommandArgument);
+            //boEmpleado.eliminar(idEmpleado);
+            Response.Redirect("ListarOrdenes.aspx");
+        }
+
+        protected void lbVisualizar_Click(object sender, EventArgs e)
+        {
+            int idEmpleado = Int32.Parse(((LinkButton)sender).CommandArgument);
+            //Empleado empleado = empleados.SingleOrDefault(x => x.IdPersona == idEmpleado);
+            //Session["empleadoSeleccionado"] = empleado;
+            Response.Redirect("RegistrarOrden.aspx?accion=ver");
+        }
+
+        protected void lbBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }

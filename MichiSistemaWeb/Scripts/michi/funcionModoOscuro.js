4422de068be871aaa716b50bee05617abac8f81a
PapaHuayro <jackcito20190167@gmail.com>
2025-06-29 19:04:14 -0500

    funcion script modo oscuro.js

---
+++ MichiSistemaWeb/Scripts/michi/funcionModoOscuro.js
@@ -0,0 +1,10 @@
+﻿// Aplicar el tema inmediatamente para evitar flash, se llama desde aqui para evitar errores
+(function () {
+    const theme = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (theme === 'dark' || (!theme && prefersDark)) {
+        document.body.classList.add('dark-mode');
+        document.getElementById('switchMode')?.checked = true;
+    }
+})();
